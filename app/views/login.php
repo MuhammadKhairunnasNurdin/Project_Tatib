5b11664c35a64f7f3463bd716b0c7622b098f71e
Lukman <lukmaneka216@gmail.com>
2023-11-28 23:12:00 +0700

    nothing update

--- app/views/login.php
+++ app/views/login.php
@@ -60,7 +60,7 @@
 <nav class="navbar navbar-expand-lg navbar-biru bg-biru">
     <div class="container-fluid">
         <a class="navbar-brand" href="#" style="color: white; font-family: 'Tiro Bangla', sans-serif;">
-            <img src="assets/img/logo_polinema.png" alt="Logo" style="width: 50px; height: 50px; margin-right: 1px; size: 10px"> Politeknik Negeri Malang
+            <img src="<?= BASEURL; ?>/img/logo_polinema.png" alt="Logo" style="width: 50px; height: 50px; margin-right: 1px; size: 10px"> Politeknik Negeri Malang
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -84,12 +84,12 @@
 
     <section class="container-fluid text-center" style="height: 80vh;">
         <div class="row" style="height: 80%">
-            <div class="col d-flex justify-content-center align-items-center" style="width:80%; height:vh; background-color: #0D366B; opacity: 1; background-image: url(assets/img/login3.jpeg); background-size:100% 100%;">
+            <div class="col d-flex justify-content-center align-items-center" style="width:80%; height:vh; background-color: #0D366B; opacity: 1; background-image: url(<?= BASEURL; ?>/img/login_kiri.png); background-size:100% 100%;">
             </div>
-            <div class="col d-flex justify-content-center align-items-center" style="width:10%; height:100vh; background-color: #0D366B; opacity: 1; background-image: url(assets/img/jti3.jpg); background-size:100% 100%;">
+            <div class="col d-flex justify-content-center align-items-center" style="width:10%; height:100vh; background-color: #0D366B; opacity: 1; background-image: url(<?= BASEURL; ?>/img/jti3.jpg); background-size:100% 100%;">
             <form id="loginForm" class="d-flex flex-column gap-2 w-10 px-1 py-1 bg-white rounded-3" method="post">
                 <div class="text-center logo-text-container">
-                    <img src="assets/img/logo_jti_baru.png" style="width: 15%;" class="logo">
+                    <img src="<?= BASEURL; ?>/img/logo_jti_baru.png" style="width: 15%;" class="logo">
                     <h1 class="h5 mb-3 fw-bolder">Sistem Tata Tertib JTI</h1>
                     <span class="small" style="margin-top: 5px; font-family: 'Tiro Bangla';">Selamat datang di sistem tata tertib Politeknik Negeri Malang untuk mahasiswa, civitas akademika</span>
                 </div>
@@ -101,11 +101,8 @@
                     <input type="password" class="form-control form-default shadow bg-body-tetiary-rounded" id="floatingPassword" placeholder="Password" name="password" required>
                     <label for="floatingPassword" class="small label-hitam">Password</label>
                 </div>
-                <div class="d-flex justify-content-between">
-                    <div class="form-check text-start">
-                        <a href="forgot_password.php" class="small label-hitam" style="color: #0D366B">Lupa Password?</a>
-                    </div>
-                    <button id="loginBtn" class="btn btn-custom-blue pt-1" type="submit" style="width: 35%;">Login</button>
+                <div class="d-flex justify-content-center">
+                    <button id="loginBtn" class="btn btn-custom-blue pt-1" type="submit" style="width: 30%;">Login</button>
                 </div>
             </form>
 
