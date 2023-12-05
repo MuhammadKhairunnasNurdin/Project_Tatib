e2b99fa2e9aee698aa8cf08e2425cb5051747801
LAPTOP-3F2K575I\Andy Nugraha Putra <andybima6@gmail.com>
2023-12-05 23:07:05 +0700

    update bang

--- app/views/mahasiswa/template/home.php
+++ app/views/mahasiswa/template/home.php
@@ -1,46 +1,46 @@
 <!--<div class="container-page">-->
 <!--    <div class="row">-->
 
-        <!-- Tampilan Dashboard  -->
-        <main class="main">
-            <div class="dashboard-box">
-                <div class="box-title">
-                    <div class="title-page">
-                        <h1 class="h2"><img class="logo-dashboard-page" src="<?=BASEURL?>/img/dash.svg" alt="">Dashboard</h1>
-                    </div>
-                    <a href="index.php" class="link-dashboard-2">
-                        <div class="refresh">
-                            <img class="logo-refresh" src="<?=BASEURL?>/img/reload_.svg" alt="Refresh">
-                            <p>
-                                Refresh
-                            </p>
-                        </div>
-                    </a>
-                </div>
+<!-- Tampilan Dashboard  -->
+<main class="main">
+    <div class="dashboard-box">
+        <div class="box-title">
+            <div class="title-page">
+                <h1 class="h2"><img class="logo-dashboard-page" src="<?= BASEURL ?>/img/dash.svg" alt="">Dashboard</h1>
             </div>
-            <div class="welcome-box">
-                <h1 class="welcome-title">Selamat Datang, Andy Nugraha Putra</h1>
-                <p class="welcome-subtitle">Selamat Datang di Website Tata Tertib Mahasiswa Jurusan</p>
-                <p style="color: #3D55AB"><b>Teknologi Informasi</b></p>
-            </div>
-            <div class="content">
-                <div class="card sanction-report">
-                    <h2>Laporan Sanksi</h2>
-                    <p>Silakan cek tingkatan jenis Pelanggaran di sini:</p>
-                    <a href="index.php?page=jenistatib">
-                        <button class="rules-button">Tata Tertib</button>
-                    </a>
+            <a href="index.php" class="link-dashboard-2">
+                <div class="refresh">
+                    <img class="logo-refresh" src="<?= BASEURL ?>/img/reload_.svg" alt="Refresh">
+                    <p>
+                        Refresh
+                    </p>
                 </div>
-                <div class="card student-info">
-                    <img src="<?=BASEURL?>/img/Andy_Nugraha.jpeg" alt="User" class="student-photo">
-                    <h3>Andy Nugraha Putra</h3>
-                    <p class="student-id">2241720006</p>
-                    <p class="student-location">Malang, Jawa Timur</p>
-                    <p class="student-status">Status Anda: <span class="status-active">Mahasiswa Aktif</span></p>
-                </div>
-            </div>
-        </main>
+            </a>
+        </div>
     </div>
+    <div class="welcome-box">
+        <h1 class="welcome-title">Selamat Datang, Andy Nugraha Putra</h1>
+        <p class="welcome-subtitle">Selamat Datang di Website Tata Tertib Mahasiswa Jurusan</p>
+        <p style="color: #3D55AB"><b>Teknologi Informasi</b></p>
+    </div>
+    <form action="<?= BASEURL ?>/Mahasiswa/module" method="POST">
+        <div class="content">
+            <div class="card sanction-report">
+                <h2>Laporan Sanksi</h2>
+                <p>Silakan cek tingkatan jenis Pelanggaran di sini:</p>
+                <button name="page" value="jenistatib" type="submit" class="rules-button">Tata Tertib</button>
+            </div>
+            <div class="card student-info">
+                <img src="<?= BASEURL ?>/img/Andy_Nugraha.jpeg" alt="User" class="student-photo">
+                <h3>Andy Nugraha Putra</h3>
+                <p class="student-id">2241720006</p>
+                <p class="student-location">Malang, Jawa Timur</p>
+                <p class="student-status">Status Anda: <span class="status-active">Mahasiswa Aktif</span></p>
+            </div>
+        </div>
+    </form>
+</main>
+</div>
 </div>
 </body>
 
