adf90ac612c3eabfd83aa20bf15aff9a87c39a72
halurabiyyu <halurabiyyu@gmail.com>
2023-12-06 09:40:10 +0700

    ADMIN :: Update Tampilan Fitur Add Edit Dosen dan Mahasiswa

--- app/views/admin/module/mahasiswa/edit/index.php
+++ app/views/admin/module/mahasiswa/edit/index.php
@@ -1,67 +1,65 @@
 <?php
 ?>
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" style="background-image: url(<?= BASEURL?>/img/gedung-jti.jpg); background-repeat: no-repeat; background-size:cover;">
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
         <h1 class="h2">Mahasiswa</h1>
     </div>                
     <form action="fungsi/edit.php?anggota=edit" method="POST">
         <div class="row">
             <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-header">
+                <div class="card" style="border: none;">
+                    <div class="card-header bg-white title-form-mahasiswa">
                         Form Edit Mahasiswa
                     </div>
                 <div class="card-body">
                     <input type="hidden" value="<?php echo $row['user_id']; ?>" name="id">
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="nip" class="form-label">NIM:</label>
-                        <input type="text" name="nip" class="form-control" value="<?= $row['nip']; ?>">
+                        <input type="text" name="nip" class="form-control" value="2341720072">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="nama" class="form-label">Nama:</label>
-                        <input type="text" name="nama" class="form-control" value="<?= $row['nama']; ?>">
+                        <input type="text" name="nama" class="form-control" value="Halur Muhammad Abiyyu">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="kelas" class="form-label">Kelas:</label>
-                        <input type="text" name="ttl" class="form-control" value="<?= $row['nama']; ?>">
+                        <input type="text" name="ttl" class="form-control" value="TI 2E">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="ttl" class="form-label">Tanggal Lahir:</label>
-                        <input type="text" name="ttl" class="form-control" value="<?= $row['nama']; ?>">
+                        <input type="date" name="ttl" class="form-control" value="05/07/2003">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="recipient-name" class="col-form-label">Jenis Kelamin:</label>
-                        <br>
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" <?=($row['jenis_kelamin'] === "L") ? 'checked' : ''?>>
+                            <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" checked>
                             <label for="inlineRadio1" class="form-check-label">Laki-laki</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" name="jenis_kelamin" value="P" <?=($row['jenis_kelamin'] === "P") ? 'checked' : ''?>>
+                            <input type="radio" class="form-check-input" name="jenis_kelamin" value="P">
                             <label for="inlineRadio2" class="form-check-label">Perempuan</label>
                         </div>
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="alamat" class="form-label">Alamat</label>
-                        <textarea type="text" name="alamat" class="form-control"><?= $row['alamat']; ?></textarea>
+                        <textarea type="text" name="alamat" class="form-control" value="Lowokwaru, Malang"></textarea>
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-2">
                         <label for="recipient-name" class="form-label">No Telepon</label>
-                        <input type="number" name="no_telp" class="form-control" value="<?= $row['no_telp']; ?>">
+                        <input type="number" name="no_telp" class="form-control" value="0892345212">
                     </div>
                 </div>
             </div>
         </div>
             <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-header">
-                        Form Edit Login Dosen
+                <div class="card" style="border: none;">
+                    <div class="card-header bg-white  title-form-mahasiswa">
+                        Form Edit Login Mahasiswa
                     </div>
                     <div class="card-body">
-                        <hr class="border border-primary border-3 opacity-75">
                         <div class="mb-3">
                             <label for="username" class="form-label">Username:</label>
-                            <input type="text" name="username" class="form-control" value="<?= $row['username']; ?>">
+                            <input type="text" name="username" class="form-control">
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
@@ -70,21 +68,14 @@
                                 Kosongi password, jika tidak ingin menggantinya
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-body" style="text-align: center;">
-                        <button class="btn btn-primary" type="submit"><i class="fa fa-floppy-o" aria-hidden= 'true'></i>
-                            Ubah
-                        </button>
-                        <a href="index.php?page=anggota" class="btn btn-primary"><i class="fa fa-times"></i>
-                            Batal
-                        </a>
+                        <div class="mb-2" style="text-align: center;">
+                            <button class="btn btn-dark-blue" type="submit"><i class="fa fa-floppy-o" aria-hidden= 'true'></i>
+                                Ubah
+                            </button>
+                            <button class="btn btn-danger"><i class="fa fa-times"></i>
+                                Batal
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
