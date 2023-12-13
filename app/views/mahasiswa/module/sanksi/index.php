9573ac5d54ffbaa19b2a57dba2f9e2ecdfd1ab96
LAPTOP-3F2K575I\Andy Nugraha Putra <andybima6@gmail.com>
2023-12-13 20:37:52 +0700

    Mahasiswa clear boss

--- app/views/mahasiswa/module/sanksi/index.php
+++ app/views/mahasiswa/module/sanksi/index.php
@@ -1,4 +1,48 @@
 <style>
+    /* table {
+        width: 80%;
+        border-collapse: collapse;
+        margin: auto;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+        background-color: white;
+    }
+
+    th {
+        background-color: #0D366B;
+        color: white;
+    } */
+
+    /* .box-sanksi {
+        width: 125px;
+        height: 39px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 15px;
+        background-color: #2059A2;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        margin: auto;
+        transition: background-color 0.3s;
+    }
+
+    .box-sanksi:hover {
+        background-color: #0B2A53;
+    } */
+
+    /* tingkat 1,2,3,4,5 */
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
     .logo-container {
         display: flex;
         align-items: center;
@@ -18,89 +62,169 @@
         text-align: center;
         font-family: Anek Telugu;
         font-size: 32px;
-        font-style: normal;
         font-weight: 600;
-        line-height: normal;
         margin-top: 30px;
     }
 
-    table {
-        width: 80%;
-        border-collapse: collapse;
-        margin: auto;
-    }
-
-    table,
-    th,
-    td {
-        border: 1px solid black;
-        padding: 8px;
-        text-align: left;
-        background-color: white;
-    }
-
-    th {
-        background-color: #0D366B;
-        color: white;
-    }
-
-    .box-sanksi {
-        width: 125px;
-        height: 39px;
-        display: flex;
+    .container-contents {
+        width: 928px;
+        flex-shrink: 0;
         justify-content: center;
-        align-items: center;
+        align-items: flex-start;
         border-radius: 15px;
-        background-color: #2059A2;
-        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        background: rgba(255, 255, 255, 0.80);
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 25px;
         margin: auto;
-        transition: background-color 0.3s;
+        margin-top: 15px;
+        padding: 15px;
     }
 
-    .box-sanksi:hover {
-        background-color: #0B2A53;
+    .cards {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        gap: 10px;
+        height: 45px;
+        margin-top: 15px;
+    }
+
+    .bold {
+        font-weight: bold;
+    }
+
+    .jarak {
+        margin-top: 15px;
+    }
+
+    .text {
+        margin-right: 10px;
+    }
+
+    .box-pelanggaran {
+        background-color: white;
+        width: 405px;
+        height: 45px;
+        border-radius: 8px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+
     }
 
     .text-sanksi {
         color: white;
         text-decoration: none;
         font-family: Anek Telugu;
-        font-size: 20px;
+        font-size: 40px;
         font-style: normal;
         font-weight: 600;
         line-height: normal;
         margin: auto;
+        width: 928px;
     }
 
-    .popup-container {
-        display: none;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.5);
+    .text,
+    .bold {
+        margin-top: 55px;
+    }
+
+    .box-sanksi {
+        margin-bottom: 12px;
+    }
+
+    .box-isi {
+        margin-bottom: 40px;
+
+    }
+
+    .box-pelanggaran {
+
+        text-align: center;
+        margin-left: 220px;
+        margin-top: -30px;
+    }
+
+    /* Tingkat 2 */
+    .kirim-bukti {
+        width: 129px;
+        height: 51px;
+        flex-shrink: 0;
+        display: flex;
         justify-content: center;
         align-items: center;
-        z-index: 1;
+        border-radius: 15px;
+        background-color: #2EA62C;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        margin: auto;
+        transition: background-color 0.3s;
     }
 
-    .popup-box {
-        fill: rgba(255, 255, 255, 0.80);
-        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-        padding: 20px;
-        border-radius: 10px;
-        width: 928px;
-        height: 699px;
-        text-align: center;
+    .kirim-bukti:hover {
+        background-color: #298527;
     }
 
-    .close-popup {
-        cursor: pointer;
-        color: #FF2121;
-        font-weight: bold;
-        margin-left: 95%;
+    .text-bukti {
+        width: 126px;
+        height: 39px;
+        flex-shrink: 0;
+        color: #FFF;
+        font-family: Poppins;
         font-size: 20px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+        cursor: pointer;
+        margin-top: 25px;
+    }
+
+    .posisi-bukti {
+        margin-left: 200px;
+    }
+
+    /* tingkat 5,4,3 */
+    .bold-file {
+        display: flex;
+        width: 336px;
+        height: 65px;
+        flex-direction: column;
+        justify-content: center;
+        flex-shrink: 0;
+        color: #000;
+
+        font-family: Poppins;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+    }
+
+    .download-bukti {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 1000000000000000;
+        line-height: normal;
+        color: white;
+
+    }
+
+    .kirim-file {
+        border-radius: 15px;
+        background: #0D366B;
+        box-shadow: 6px 6px 4px 0px rgba(0, 0, 0, 0.25);
+        font-size: 10px;
+        cursor: pointer;
+        margin-left: 40px;
+    }
+
+    .kirim-file:hover {
+
+        background: #0B2950;
+        box-shadow: 6px 6px 4px 0px rgba(0, 0, 0, 0.25);
+        cursor: pointer;
+
     }
 </style>
 
@@ -109,8 +233,13 @@
     <div class="main">
         <div class="logo-container">
             <img class="logo-jenis-sanksi" src="<?= BASEURL ?>/img/sanksi.svg" alt="Jenis Logo">
-            <p class="Text-judul-sanksi">Sanksi</p>
+            <form action="<?= BASEURL ?>" method="POST">
+                <p class="Text-judul-sanksi">Sanksi</p>
         </div>
+
+
+
+
 
         <table>
 
@@ -128,7 +257,7 @@
                 <td>Tingkat 1</td>
                 <td>18 / Okt / 2023</td>
                 <td>
-                    <button class="box-sanksi" >
+                    <button name="page" type="submit" class="box-sanksi">
                         <p>
                             <a class="text-sanksi">
                                 Cek Disini!
@@ -143,7 +272,7 @@
                 <td>Tingkat 2</td>
                 <td>18 / Okt / 2023</td>
                 <td>
-                    <button class="box-sanksi" >
+                    <button class="box-sanksi">
                         <p>
                             <a class="text-sanksi">
                                 Cek Disini!
@@ -173,7 +302,7 @@
                 <td>Tingkat 4</td>
                 <td>18 / Okt / 2023</td>
                 <td>
-                    <button class="box-sanksi" >
+                    <button class="box-sanksi">
                         <p>
                             <a class="text-sanksi">
                                 Cek Disini!
@@ -188,7 +317,7 @@
                 <td>Tingkat 5</td>
                 <td>18 / Okt / 2023</td>
                 <td>
-                    <button class="box-sanksi" >
+                    <button class="box-sanksi">
                         <p>
                             <a class="text-sanksi">
                                 Cek Disini!
@@ -200,8 +329,8 @@
 
         </table>
     </div>
-   
 
-    <script src="test.js"></script>
+
+
 
 </body>
