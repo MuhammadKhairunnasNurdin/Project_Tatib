2d44708626c55be2b72f7518c80aeaa9faef4ebb
LAPTOP-3F2K575I\Andy Nugraha Putra <andybima6@gmail.com>
2023-12-13 20:37:52 +0700

    Mahasiswa clear boss

---
+++ app/views/mahasiswa/module/sanksi/input/sanksi-2.php
@@ -0,0 +1,123 @@
+<div class="container-page">
+    <div class="row">
+        <style>
+            .logo-container {
+                display: flex;
+                align-items: center;
+
+            }
+
+            .logo-jenis-sanksi {
+                width: 30px;
+                /* Sesuaikan ukuran logo sesuai kebutuhan */
+                height: auto;
+                margin-right: 40px;
+                font-size: 24px;
+                margin-left: 40px;
+                color: #363637;
+                /* Sesuaikan jarak antara logo dan teks */
+            }
+
+            .Text-judul-sanksi {
+                color: #403737;
+                text-align: center;
+                font-family: Anek Telugu;
+                font-size: 32px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: normal;
+                margin-top: 30px;
+                /* Sesuaikan ukuran font sesuai kebutuhan */
+            }
+
+            table {
+
+                width: 80%;
+
+                border-collapse: collapse;
+                margin: auto;
+            }
+
+
+            table,
+            th,
+            td {
+
+                border: 1px solid black;
+
+                padding: 8px;
+
+                text-align: left;
+                background-color: white;
+            }
+
+
+            th {
+
+                background-color: #0D366B;
+                color: white;
+            }
+        </style>
+
+        <div class="main">
+            <div class="logo-container">
+                <img class="logo-jenis-sanksi" src="img\sanksi.svg" alt="Jenis Logo">
+                <p class="Text-judul-sanksi">Sanksi</p>
+            </div>
+
+            
+        <!-- Tingkat 2 -->
+        <!-- <div class="container-contents">
+            <div class="box-isi">
+                <p>
+                <h1 class="text-sanksi" style="margin-bottom: 15px;">
+                    <b style="color: black">Pelanggaran Tingkat 2</b>
+                </h1>
+            </div>
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 60px;">Jenis Pelanggaran</a>:
+                <a class="text">Bermain kartu dan game online di area kampus</a>
+
+            </div>
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 40px;">Tingkat Pelanggaran </a>:
+                <a>Tingkat 3</a>
+
+            </div>
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 49px;">Sanksi Pelanggaran</a>:
+                <a>Kayang 100x</a>
+            </div>
+
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 40px;">Tanggal Pelanggaran</a>:
+                <a>18 / Okt / 2023</a>
+            </div>
+
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 45px; ">Bukti Pelanggaran * </a>:
+                <div class="box-pelanggaran">
+                </div>
+            </div>
+
+            <div class="box-isi">
+                <a class="bold" style="margin-right: 45px; ">Bukti Kompensasi * </a>:
+                <div class="box-pelanggaran">
+                </div>
+            </div>
+
+            <div class="posisi-bukti">
+                <button class="kirim-bukti">
+                    <h1 class="text-bukti">
+                        Kirim Bukti
+                    </h1>
+                </button>
+            </div>
+
+            </a>
+        </div>
+        </p>
+    </div>
+    </div>
+</body> -->
+
