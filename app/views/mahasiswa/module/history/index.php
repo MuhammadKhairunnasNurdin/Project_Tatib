5add1adffed7bcf161042729a64ffc62ee8a1bdc
Lukman <lukmaneka216@gmail.com>
2023-12-15 23:57:13 +0700

    update page mahasiswa

--- app/views/mahasiswa/module/history/index.php
+++ app/views/mahasiswa/module/history/index.php
@@ -1,28 +1,46 @@
-        <div class="main">
-            <div class="logo-container">
-                <img class="logo-jenis-sanksi" src="<?=BASEURL?>/img/history.svg" alt="Jenis Logo">
-                <p class="Text-judul-sanksi">History</p>
-            </div>
-
-            <body>
-
-                <table>
-
-                    <tr>
-                        <th>No</th>
-                        <th>Jenis Sanksi</th>
-                        <th>Tingkat Pelanggaran</th>
-                        <th>Tanggal Kejadian</th>
-                        <th>Tanggal Penyelesaian Sanksi</th>
-                        <th>Validasi</th>
-                    </tr>
-
-                    <tr>
-                        <td>1</td>
-                        <td>Bermain kartu, game online</td>
-                        <td>Tingkat 3</td>
-                        <td>18 / Okt / 2023</td>
-                        <td>20 / Okt / 2023</td>
-                        <td><a href="#"></a></td>
-                    </tr>
-                </table>
+<div class="main">
+    <div class="logo-container">
+        <img class="logo-jenis-sanksi" src="<?=BASEURL?>/img/history.svg" alt="Jenis Logo">
+        <p class="Text-judul-sanksi">History</p>
+    </div>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Jenis Sanksi</th>
+            <th>Tingkat Pelanggaran</th>
+            <th>Tanggal Kejadian</th>
+            <th>Tanggal Penyelesaian Sanksi</th>
+            <th>Validasi</th>
+        </tr>
+        <?php $id = 1;
+        $data = [
+	        "history" => [
+		        [
+			        'jenis_pelanggaran' => 'Bermain Game',
+			        'tingkat_pelanggaran' => 'Tingkat 3',
+			        'tgl_pelanggaran' => '2023-12-02',
+			        'tgl_validasi' => '2023-12-05',
+		        ],
+		        [
+			        'jenis_pelanggaran' => 'Menggunakan Narkoba',
+			        'tingkat_pelanggaran' => 'Tingkat 1',
+			        'tgl_pelanggaran' => '2023-12-12',
+			        'tgl_validasi' => 'NULL',
+		        ]
+	        ]
+        ];
+        foreach ($data['history'] as $htry):
+            if ($htry['tgl_validasi'] == 'NULL'){
+                continue;
+            }
+            ?>
+        <tr>
+            <td><?=$id?></td>
+            <td><?=$htry['jenis_pelanggaran']?></td>
+            <td><?=$htry['tingkat_pelanggaran']?></td>
+            <td><?=$htry['tgl_pelanggaran']?></td>
+            <td><?=$htry['tgl_validasi']?></td>
+            <td><a href="#"></a></td>
+        </tr>
+        <?php $id++; endforeach; ?>
+    </table>
