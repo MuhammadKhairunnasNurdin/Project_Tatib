c6b2ce702b414faf51909ba60669d8d364d5ead3
Lukman <lukmaneka216@gmail.com>
2023-12-16 21:26:31 +0700

    debugging login alert

--- app/controllers/Authorization.php
+++ app/controllers/Authorization.php
@@ -37,7 +37,7 @@ class Authorization extends Controller
 
 			/*if some error in login occur*/
 			if (count($loginLocation) === 3) {
-				$_SESSION["flashMessage"] = $loginLocation["errorMessage"];
+				$_SESSION["flashMessage"]["login"] = $loginLocation["errorMessage"];
 				header("Location: " . BASEURL . "/$controller/$method");
 				return;
 			}
